2cc96c92cff2154b5123ba6f6b8f87400a6f3e5c
Samim Aftab Ahmed <samimaftabahmed@gmail.com>
2024-07-03 17:12:13 +0530

    Creating Partitions with Equal elements

---
+++ src/main/java/org/samim/EqualPartitionCreator.java
@@ -0,0 +1,52 @@
+package org.samim;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class EqualPartitionCreator {
+
+    public static void main(String[] args) {
+        int totalElements = 6083;
+        int maxPartitions = 20;
+        List<Integer> inputList = new ArrayList<>(totalElements);
+        IntStream.range(0, totalElements).forEach(inputList::add);
+
+        List<Integer> subListFirst = inputList;
+        int threshold = 1;
+        List<Integer> subListLast = null;
+        if (totalElements > maxPartitions) {
+            int remainder = totalElements % maxPartitions;
+            int difference = totalElements - remainder;
+            threshold = difference / maxPartitions;
+            System.out.println("Remainder: " + remainder);
+            System.out.println("Threshold: " + threshold);
+
+            subListFirst = inputList.subList(0, difference);
+            subListLast = inputList.subList(difference, totalElements);
+
+            System.out.println("sublist 1st: " + subListFirst.size());
+            System.out.println("sublist 2nd: " + subListLast.size());
+        }
+
+        List<List<Integer>> nonModifiablePartitions = Lists.partition(subListFirst, threshold);
+        List<List<Integer>> finalPartitions = new ArrayList<>(nonModifiablePartitions);
+        System.out.println("Partitions: " + finalPartitions.size());
+
+        if (subListLast != null) {
+            for (int j = 0; j < subListLast.size(); j++) {
+                Integer item = subListLast.get(j);
+                List<Integer> newPartition = new ArrayList<>(finalPartitions.get(j));
+                newPartition.add(item);
+                finalPartitions.set(j, newPartition);
+            }
+        }
+
+        for (int i = 0; i < finalPartitions.size(); i++) {
+            System.out.println("Partition " + (i + 1) + " size: " + finalPartitions.get(i).size());
+        }
+    }
+
+}
