6ab3a6d6f72466930c24421d7375ee54aa209756
Samim Aftab Ahmed <samimaftabahmed@gmail.com>
2024-08-15 23:32:27 +0530

    print line moved

--- src/main/java/org/samim/design_patterns/factory/FactoryMain.java
+++ src/main/java/org/samim/design_patterns/factory/FactoryMain.java
@@ -16,16 +16,14 @@ public class FactoryMain {
     private static void executeFactoryMain() {
         OperatingSystem win8 = OperatingSystemFactory.getInstance(OS.WINDOWS, "8.1", "x86");
         OperatingSystem ubuntu20_04 = OperatingSystemFactory.getInstance(OS.LINUX, "Ubuntu 20.04 LTS", "x64");
+
         String directory = "office_files";
-
-        System.out.printf("---%s %s commands---\n", win8.getType().getOs(), win8.getVersion());
         viewCommands(win8, directory);
-
-        System.out.printf("\n---%s %s commands---\n", ubuntu20_04.getType().getOs(), ubuntu20_04.getVersion());
         viewCommands(ubuntu20_04, directory);
     }
 
     private static void viewCommands(OperatingSystem os, String dir) {
+        System.out.printf("\n---%s %s commands---\n", os.getType().getOs(), os.getVersion());
         os.changeDirectory(dir);
         os.createDirectory(dir);
         os.removeDirectory(dir);
